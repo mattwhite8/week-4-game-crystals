04a35d452b7402bb47da65326ef431855d197e99
Matt White <mattwhite1337@gmail.com>
2017-06-25 23:42:23 -0400

    ran eslint

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,57 +2,57 @@
 
 $( document ).ready(function() {
 
-  var score = 0;
-  var wins = 0;
-  var losses = 0;
-  const chime = $("#chime");
+    var score = 0;
+    var wins = 0;
+    var losses = 0;
+    var chime = $("#chime");
 
 
-  function randomNumberToGet() {
-    return Math.floor(Math.random() * (120 - 19) + 19);
-  }
-
-  function randomCrystalNumber() {
-    return Math.floor(Math.random() * (12 - 1) + 1);
-  }
-
-  function setRandom() {
-    $(".gem").map(function(){
-      $(this).data("number", randomCrystalNumber());
-      console.log($(this).data());
-    })
-  }
-
-
-  var target = randomNumberToGet();
-  
-  $("#target").html(target);
-  
-  setRandom();
-  
-  $(".gem").click(function(){
-    chime.get(0).play();
-    score += $(this).data("number");
-    $("#your-score").html(score);
-    console.log(score);
-    
-    if (score === target) {
-      wins++;
-      $("#wins").html("Wins: " + wins);
-      score = 0;
-      target = randomNumberToGet();
-      $("#your-score").html(score);
-      $("#target").html(target);
-      setRandom();
-    } else if (score > target){
-      losses++;
-      $("#losses").html("Losses: " + losses);
-      score = 0;
-      target = randomNumberToGet();
-      $("#your-score").html(score);
-      $("#target").html(target);
-      setRandom();
+    function randomNumberToGet() {
+        return Math.floor(Math.random() * (120 - 19) + 19);
     }
-  })
+
+    function randomCrystalNumber() {
+        return Math.floor(Math.random() * (12 - 1) + 1);
+    }
+
+    function setRandom() {
+        $(".gem").map(function(){
+            $(this).data("number", randomCrystalNumber());
+            //console.log($(this).data());
+        });
+    }
+
+
+    var target = randomNumberToGet();
   
-})
+    $("#target").html(target);
+  
+    setRandom();
+  
+    $(".gem").click(function(){
+        chime.get(0).play();
+        score += $(this).data("number");
+        $("#your-score").html(score);
+        //console.log(score);
+    
+        if (score === target) {
+            wins++;
+            $("#wins").html("Wins: " + wins);
+            score = 0;
+            target = randomNumberToGet();
+            $("#your-score").html(score);
+            $("#target").html(target);
+            setRandom();
+        } else if (score > target){
+            losses++;
+            $("#losses").html("Losses: " + losses);
+            score = 0;
+            target = randomNumberToGet();
+            $("#your-score").html(score);
+            $("#target").html(target);
+            setRandom();
+        }
+    });
+  
+});
